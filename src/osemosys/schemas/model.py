3c1524b492da8ff49a6159c9f3292fc73398a693
edwardxtg <edwardxtg@gmail.com>
2023-10-17 15:34:34 +0100

    add reserve margins and other global data to RunSpec class

--- src/osemosys/schemas/model.py
+++ src/osemosys/schemas/model.py
@@ -15,11 +15,10 @@ class RunSpec(OSeMOSYSBase):
 
     # financials
     depreciation_method: RegionData | None # 1= "straight-line" # or "sinking-fund"
-    discount_rate: RegionTechnologyYearData | None # may want to express this at the technology or model level
+    discount_rate: RegionData | None # may want to express this at the technology or model level
 
     # time definition
-    #TODO
-    #time_definition: TimeDefinition
+    time_definition: TimeDefinition
 
     # nodes
     regions: List[Region]
@@ -28,8 +27,7 @@ class RunSpec(OSeMOSYSBase):
     commodities: List[Commodity]
 
     # impact constraints (e.g. CO2)
-    #TODO
-    #impacts: List[Impact]
+    impacts: List[Impact]
 
     # technologies
     #TODO
@@ -38,12 +36,15 @@ class RunSpec(OSeMOSYSBase):
     #transmission_technologies: List[TechnologyTransmission]
 
     # reserve margins if any
-    reserve_margins_commodity: List[Dict[str,RegionYearData]] | None
-    reserve_margins_technology: List[Dict[str, RegionYearData]] | None
+    reserve_margins_level: RegionYearData | None
+    reserve_margins_commodity: RegionCommodityYearData | None
+    reserve_margins_technology: RegionTechnologyYearData | None
 
     # renewable targets
     renewable_targets: RegionYearData | None
     
+    # Default values
+    #TODO
 
     def to_simplicity(self, comparison_directory) -> Dict[str,str]:
         """
@@ -68,20 +69,31 @@ class RunSpec(OSeMOSYSBase):
 
         def get_depreciation_method(root_dir):
             df = pd.read_csv(os.path.join(root_dir, 'DepreciationMethod.csv'))
-            
-
+            return df if not df.empty else None 
+        
         def get_discount_rate(root_dir):
-            None
+            df = pd.read_csv(os.path.join(root_dir, 'DiscountRate.csv'))
+            return df if not df.empty else None
+        
+        def get_reserve_margins_level(root_dir):
+            df = pd.read_csv(os.path.join(root_dir, 'ReserveMargin.csv'))
+            return df if not df.empty else None
+        
         def get_reserve_margins_commodity(root_dir):
-            None
+            df = pd.read_csv(os.path.join(root_dir, 'ReserveMarginTagFuel.csv'))
+            return df if not df.empty else None
+        
         def get_reserve_margins_technology(root_dir):
-            None
+            df = pd.read_csv(os.path.join(root_dir, 'ReserveMarginTagTechnology.csv'))
+            return df if not df.empty else None
+        
         def get_renewable_targets(root_dir):
-            None
+            df = pd.read_csv(os.path.join(root_dir, 'REMinProductionTarget.csv'))
+            return df if not df.empty else None
 
-        # depreciation method
         depreciation_method = get_depreciation_method(root_dir)
         discount_rate = get_discount_rate(root_dir)
+        reserve_margins_level = get_reserve_margins_level(root_dir)
         reserve_margins_commodity = get_reserve_margins_commodity(root_dir)
         reserve_margins_technology = get_reserve_margins_technology(root_dir)
         renewable_targets = get_renewable_targets(root_dir)
@@ -93,12 +105,15 @@ class RunSpec(OSeMOSYSBase):
             description = None,
             depreciation_method = depreciation_method,
             discount_rate=discount_rate,
+            impacts=Impact.from_simplicity(root_dir=root_dir),
             regions=Region.from_simplicity(root_dir=root_dir),
             #TODO
             #production_technologies=TechnologyProduction.from_simplicity(root_dir=root_dir),
             #storage_technologies=TechnologyStorage.from_simplicity(root_dir=root_dir),
             #transmission_technologies=TechnologyTransmission.from_simplicity(root_dir=root_dir),
             commodities=Commodity.from_simplicity(root_dir=root_dir),
+            time_definition=TimeDefinition.from_simplicity(root_dir=root_dir),
+            reserve_margins_level=reserve_margins_level, 
             reserve_margins_commodity=reserve_margins_commodity,
             reserve_margins_technology=reserve_margins_technology,
             renewable_targets=renewable_targets
